260d90b3f5aebc0b0316c6d4ca6b5443e5690260
Jared <jaredtponting@gmail.com>
2024-09-06 15:36:50 +0100

    refactor

--- app/game/pieces.py
+++ app/game/pieces.py
@@ -1,22 +1,30 @@
+import copy
 from typing import Union, List
 
 from app import get_piece_asset_path
+from abc import ABC, abstractmethod
 
 import pygame
 import os
 
-from . import Colour
+from . import Colour, is_in_bounds
 
 
-class Piece:
-    def __init__(self, position, colour, sprite_path, square_size):
+class Piece(ABC):
+    def __init__(self, position: tuple[int, int], colour: Colour, sprite_path: str, square_size: int):
         self.actual_y = None
         self.actual_x = None
         self.position = position
         self.colour = colour
         self.has_moved = False
         self.square_size = square_size
-        self.sprite = pygame.transform.scale(pygame.image.load(sprite_path), (0.5 * square_size, square_size))
+
+        # Load sprite with error handling
+        try:
+            self.sprite = pygame.transform.scale(pygame.image.load(sprite_path), (0.5 * square_size, square_size))
+        except pygame.error as e:
+            print(f"Error loading sprite: {e}")
+            self.sprite = None
 
     def render(self, screen, position):
         piece_x, piece_y = position
@@ -24,10 +32,11 @@ class Piece:
         self.actual_y = position[1]
         screen.blit(self.sprite, (piece_y, piece_x))
 
-    def move(self, new_position):
+    def move(self, new_position: tuple[int, int]):
         self.position = new_position
         self.has_moved = True
 
+    @abstractmethod
     def valid_moves(self, board):
         """
         Checks board for all valid moves for current piece
@@ -69,19 +78,19 @@ class Pawn(Piece):
 
         # Move PAWN one square forward
         next_ROW = ROW + self.direction
-        if self.is_in_bounds(next_ROW, COL) and board[next_ROW][COL] is None:
+        if is_in_bounds(next_ROW, COL) and board[next_ROW][COL] is None:
             moves.append((next_ROW, COL))
 
         # First move only. Move PAWN two squares forward
         if not self.has_moved:
             next_2_ROW = next_ROW + self.direction
-            if self.is_in_bounds(next_2_ROW, COL) and board[next_2_ROW][COL] is None:
+            if is_in_bounds(next_2_ROW, COL) and board[next_2_ROW][COL] is None:
                 moves.append((next_2_ROW, COL))
 
         # Capture Moves
         for dcol in [-1, 1]:
             next_COL = COL + dcol
-            if self.is_in_bounds(next_ROW, next_COL) and board[next_ROW][next_COL] is not None:
+            if is_in_bounds(next_ROW, next_COL) and board[next_ROW][next_COL] is not None:
                 if board[next_ROW][next_COL].colour != self.colour:
                     moves.append((next_ROW, next_COL))
 
@@ -94,9 +103,7 @@ class Pawn(Piece):
 
         ROW, COL = self.position
         trow, tcol = target_position
-        return (trow == ROW + self.direction and (tcol == COL + 1 or tcol == COL - 1) and board[trow][
-            tcol] is not None and board[trow][
-                    tcol].colour != self.colour)
+        return trow == ROW + self.direction and (tcol == COL + 1 or tcol == COL - 1)
 
 
 class Rook(Piece):
@@ -117,7 +124,7 @@ class Rook(Piece):
                 new_row += drow
                 new_col += dcol
 
-                if not self.is_in_bounds(new_row, new_col):
+                if not is_in_bounds(new_row, new_col):
                     break
 
                 if board[new_row][new_col] is None:
@@ -145,7 +152,7 @@ class Knight(Piece):
             new_row = ROW + drow
             new_col = COL + dcol
 
-            if not self.is_in_bounds(new_row, new_col):
+            if not is_in_bounds(new_row, new_col):
                 continue
             elif board[new_row][new_col] is None:
                 moves.append((new_row, new_col))
@@ -173,7 +180,7 @@ class Bishop(Piece):
                 new_row += drow
                 new_col += dcol
 
-                if not self.is_in_bounds(new_row, new_col):
+                if not is_in_bounds(new_row, new_col):
                     break
 
                 if board[new_row][new_col] is None:
@@ -207,7 +214,7 @@ class Queen(Piece):
                 new_row += drow
                 new_col += dcol
 
-                if not self.is_in_bounds(new_row, new_col):
+                if not is_in_bounds(new_row, new_col):
                     break
 
                 if board[new_row][new_col] is None:
@@ -227,12 +234,15 @@ class King(Piece):
         super().__init__(position, colour, sprite_path, square_size)
 
     def position_is_check(self, board, new_position):
+        board[self.position[0]][self.position[1]] = None
         for row in board:
             for piece in row:
                 if piece is not None and piece.colour != self.colour:
                     if piece.can_attack(board, new_position):
+                        board[self.position[0]][self.position[1]] = self
                         return True
 
+        board[self.position[0]][self.position[1]] = self
         return False
 
     def can_attack(self, board, target_position):
@@ -243,7 +253,7 @@ class King(Piece):
         for dRow, dCol in steps:
             new_row = ROW + dRow
             new_col = COL + dCol
-            if not self.is_in_bounds(new_row, new_col):
+            if not is_in_bounds(new_row, new_col):
                 continue
             else:
                 moves.append((new_row, new_col))
@@ -260,7 +270,7 @@ class King(Piece):
             new_row = ROW + drow
             new_col = COL + dcol
 
-            if (not self.is_in_bounds(new_row, new_col)) or self.position_is_check(board, (new_row, new_col)):
+            if (not is_in_bounds(new_row, new_col)) or self.position_is_check(board, (new_row, new_col)):
                 continue
             elif board[new_row][new_col] is None:
                 moves.append((new_row, new_col))
