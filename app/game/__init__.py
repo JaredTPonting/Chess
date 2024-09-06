6f5a6875081a4e1551aaea17ef047725e9759244
Jared <jaredtponting@gmail.com>
2024-09-06 15:35:34 +0100

    is_ion_bounds funciton moved so can be reused

--- app/game/__init__.py
+++ app/game/__init__.py
@@ -4,3 +4,14 @@ from enum import Enum
 class Colour(Enum):
     WHITE = "WHITE"
     BLACK = "BLACK"
+
+
+def is_in_bounds(x: int, y: int) -> bool:
+    """
+    Checks whether the x, y position of move is within bounds of the board
+
+    :param x: Potential X position of Piece
+    :param y: Potential Y position of Piece
+    :return:  (bool) True if position is in bounds
+    """
+    return 0 <= x < 8 and 0 <= y < 8
