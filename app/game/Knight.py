f45613e7f38fb190fb66ef9465bb6150c30c0c4d
Jared <jaredtponting@gmail.com>
2024-09-07 17:04:57 +0100

    Knight cleaner code

--- app/game/Knight.py
+++ app/game/Knight.py
@@ -8,20 +8,32 @@ class Knight(Piece):
         sprite_path = get_piece_asset_path(colour, "Knight")
         super().__init__(position, colour, sprite_path, square_size)
 
-    def valid_moves(self, board):
+    def valid_moves(self, board) -> list:
+        """
+        Returns all valid moves for the Knight.
+
+        :param board: The current state of the chess board.
+        :return: A list of valid positions (as (ROW, COL)) where the Knight can move.
+        """
         moves = []
         ROW, COL = self.position
+        # Possible moves for the knight (L-shaped jumps)
         jumps = [(2, -1), (2, 1), (-2, -1), (-2, 1), (1, 2), (-1, 2), (1, -2), (-1, -2)]
 
         for drow, dcol in jumps:
-            new_row = ROW + drow
-            new_col = COL + dcol
+            new_row, new_col = ROW + drow, COL + dcol
 
             if not is_in_bounds(new_row, new_col):
+                # Skip if the move is out of bounds
                 continue
-            elif board[new_row][new_col] is None:
+
+            target_piece = board[new_row][new_col]
+
+            if target_piece is None:
+                # Move to empty square
                 moves.append((new_row, new_col))
-            elif board[new_row][new_col].colour != self.colour:
+            elif target_piece.colour != self.colour:
+                # Capture opponent's piece
                 moves.append((new_row, new_col))
 
         return moves
