163e768ab40aa8ac3e4f5d15ba738f54906badf4
Jared <jaredtponting@gmail.com>
2024-09-08 20:48:21 +0100

    Getting there

--- app/main.py
+++ app/main.py
@@ -56,6 +56,14 @@ class SingleGameState(GameState):
         self.selected_piece = None
         self.mouse_offset = (0, 0)
 
+    @staticmethod
+    def get_mouse_coords():
+        """
+        Returns mouse coordinates (row, column), rearranged to match chess board orientation.
+        :return: Tuple (row, column)
+        """
+        return pygame.mouse.get_pos()[::-1]
+
     def handle_events(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()[::-1]  # Reverse for chess orientation
@@ -65,8 +73,19 @@ class SingleGameState(GameState):
                 if piece.colour != self.board.turn:
                     print("Not your turn")
                     return
+                if not piece.move_list:
+                    print("No valid moves im afraid")
+                    return
                 self.board.selected_piece = piece
                 self.selected_piece = piece
+
+                piece_x, piece_y = piece.position
+                piece_x = self.board.board_start_x + (piece_x * self.board.square_size_x) - (
+                        self.board.square_size_x * 0.1)
+                piece_y = self.board.board_start_y + (piece_y * self.board.square_size_y) + (
+                        self.board.square_size_y * 0.25)
+
+                self.mouse_offset = (mouse_x - piece_x, mouse_y - piece_y)
 
         elif event.type == pygame.MOUSEBUTTONUP:
             if self.board.selected_piece:
@@ -83,6 +102,13 @@ class SingleGameState(GameState):
 
     def render(self):
         self.board.render(self.screen)
+        if self.board.selected_piece:
+            mouse_x, mouse_y = self.get_mouse_coords()
+
+            adjusted_x = mouse_x - self.mouse_offset[0]
+            adjusted_y = mouse_y - self.mouse_offset[1]
+
+            self.board.selected_piece.render(screen, (adjusted_x, adjusted_y))
 
 
 class GameOverState(GameState):
